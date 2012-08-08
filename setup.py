e7374a698053a0a86dd9b92f70033d44e854f39b
Derek Stegelman <dstegelman@gmail.com>
2012-08-07 20:16:12 -0500

    Add the setup.

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+version = '2.0.2'
+
+setup(name='django-bootstrap-static',
+      version=version,
+      description="A Collection of Bootstrap static",
+      long_description=open("README.md", "r").read(),
+      classifiers=[
+          "Development Status :: 5 - Production/Stable",
+          "Environment :: Web Environment",
+          "Intended Audience :: End Users/Desktop",
+          "Natural Language :: English",
+          "Operating System :: OS Independent",
+          "Framework :: Django",
+          "Programming Language :: Python",
+          "Topic :: Internet :: WWW/HTTP :: Dynamic Content :: CGI Tools/Libraries",
+          "Topic :: Utilities",
+          "License :: OSI Approved :: MIT License",
+          ],
+      keywords='',
+      author='Derek Stegelman',
+      author_email='dstegelman@gmail.com',
+      url='http://github.com/dstegelman/django-bootstrap-static',
+      license='MIT',
+      packages=find_packages(),
+      install_requires = [],
+      include_package_data=True,
+      zip_safe=False,
+    )
