4949b9df5d0cc708b270bb86547ac9982db104e7
Peter Bittner <django@bittner.it>
2017-02-05 01:10:42 +0100

    Bootstrap v3.3.7, Font Awesome v4.7.0
    Convert README (.md -> .rst) and add version badges

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
 from setuptools import setup, find_packages
 
-version = '3.3.6'
-
+version = '3.3.7'
 
 setup(name='django-bootstrap-static',
       version=version,
       description="A Collection of Bootstrap static files.",
-      long_description=open("README.md", "r").read(),
+      long_description=open("README.rst", "r").read(),
       classifiers=[
           "Development Status :: 5 - Production/Stable",
           "Environment :: Web Environment",
@@ -20,14 +19,14 @@ setup(name='django-bootstrap-static',
           "Topic :: Internet :: WWW/HTTP :: Dynamic Content :: CGI Tools/Libraries",
           "Topic :: Utilities",
           "License :: OSI Approved :: MIT License",
-          ],
+      ],
       keywords='',
       author='Derek Stegelman',
       author_email='dstegelman@gmail.com',
       url='http://github.com/dstegelman/django-bootstrap-static',
       license='MIT',
       packages=find_packages(),
-      install_requires = [],
+      install_requires=[],
       include_package_data=True,
       zip_safe=False,
-    )
+      )
